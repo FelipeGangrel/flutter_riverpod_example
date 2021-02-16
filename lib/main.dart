5f9e7757445e28494d2e0223405e89771db2078c
Felipe Gangrel <felipefrog@gmail.com>
2021-02-16 14:00:55 -0300

    applying modifiers

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,26 @@ void main() {
   );
 }
 
-final greetingProvider = Provider((ref) => 'Hello Riverpod!');
+class FakeHttpClient {
+  Future<String> get(String url) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return 'Response from $url';
+  }
+}
+
+final fakeHttpClientProvider = Provider((ref) => FakeHttpClient());
+
+// We can pass whe url for the responseProvider by using the .family modifier
+
+// If we pass the same url more than once, we are not going to see the progress
+// bar, for the value is cached
+
+// To override this behaviour, we can use the .autoDispose modifier
+final responseProvider =
+    FutureProvider.autoDispose.family<String, String>((ref, url) async {
+  final httpClient = ref.read(fakeHttpClientProvider);
+  return httpClient.get(url);
+});
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,22 +46,27 @@ class MyApp extends StatelessWidget {
 
 class MyWidget extends StatelessWidget {
   @override
-  Widget build(
-    BuildContext context,
-  ) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Riverpod example'),
       ),
-      body: Center(
-        // We wrap our text widget with a consumer
-        // This way we only rebuild the Text widget when the provider
-        // changes its value
-        child: Consumer(
-          builder: (context, watch, child) {
-            final greeting = watch(greetingProvider);
-            return Text(greeting);
-          },
+      body: Container(
+        child: Center(
+          child: Consumer(
+            builder: (context, watch, child) {
+              final responseAsyncValue =
+                  watch(responseProvider('https://testev2.com.br'));
+              return responseAsyncValue.map(
+                data: (data) => Text(data.value),
+                loading: (loading) => CircularProgressIndicator(),
+                error: (error) => Text(
+                  error.toString(),
+                  style: TextStyle(color: Colors.red),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
